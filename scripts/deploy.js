055bf2d81ec6e008aeeda6980ebbcf3f0c92d8df
kartikey <98679116+Kartikey-Tiwari@users.noreply.github.com>
2023-09-24 13:26:20 +0530

    Challenge files

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+const hre = require("hardhat");
+
+async function main() {
+  /**
+   * @dev make sure the first argument has the same name as your contract in the Hello_swtr.sol file
+   * @dev the second argument must be the message we want to set in the contract during the deployment process
+   */
+  const contract = await hre.ethers.deployContract("Pooken");
+
+  await contract.waitForDeployment();
+
+  console.log(`Swisstronik contract deployed to ${contract.target}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
